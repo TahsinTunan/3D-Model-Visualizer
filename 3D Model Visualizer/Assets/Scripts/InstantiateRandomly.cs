ac4177174f7f4162e4c010ca7a9cc8baaefd299f
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-08-08 03:46:20 +0600

    Script updated

--- 3D Model Visualizer/Assets/Scripts/InstantiateRandomly.cs
+++ 3D Model Visualizer/Assets/Scripts/InstantiateRandomly.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class InstantiateRandomly : MonoBehaviour
 {
-    // public Transform pos;
     public GameObject[] objectToInstantiate;
-
-
+    private Vector3 pos = new Vector3 (0, 0, 0);
+    private Quaternion rotation = Quaternion.Euler(0, 0, 0);
 
     void Start()
     {
         int index = Random.Range(0, objectToInstantiate.Length);
-        Instantiate(objectToInstantiate[index], new Vector3 (0, 0, 0) , objectToInstantiate[index].transform.rotation);
+        instantiateObject(index, pos, rotation);
     }
 
     
     void Update()
     {
         
+    }
+
+    void instantiateObject(int index, Vector3 pos, Quaternion rotation)
+    {
+        Instantiate(objectToInstantiate[index], pos , rotation);
     }
 }
