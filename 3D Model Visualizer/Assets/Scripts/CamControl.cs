c86e2ecf4560bf7365804c4d518e4fdb2d189f06
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-08-07 20:36:37 +0600

    Clean movement script

--- 3D Model Visualizer/Assets/Scripts/CamControl.cs
+++ 3D Model Visualizer/Assets/Scripts/CamControl.cs
@@ -6,23 +6,18 @@ using UnityEngine;
 public class CamControl : MonoBehaviour
 {
     [Header("FreeRoam Camera Settings")]
-    public float lookSpeedH = 2f;
-    public float lookSpeedV = 4f;
-    public float zoomSpeed = 10f;
-    public float dragSpeed = 6f;
-    public float panSpeed = 10f;
-    public float cameraPanSpeed = 5f;
-
+    [SerializeField] private float lookSpeedH = 2f;
+    [SerializeField] private float lookSpeedV = 4f;
+    [SerializeField] private float zoomSpeed = 10f;
+    [SerializeField] private float panSpeed = 10f;
+    [SerializeField] private float cameraPanSpeed = 5f;
     private float yaw = 0f;
     private float pitch = 0f;
 
 
-
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
+    { }
 
 
     // Update is called once per frame
@@ -30,7 +25,7 @@ public class CamControl : MonoBehaviour
     {
         //Look around with Right Mouse
         if (Input.GetMouseButton(1))
-        {
+        {    
             yaw = lookSpeedH * Input.GetAxis("Mouse X");
             pitch = lookSpeedV * Input.GetAxis("Mouse Y");
 
@@ -45,13 +40,6 @@ public class CamControl : MonoBehaviour
             transform.Translate(Camera.main.transform.right * Input.GetAxisRaw("Mouse X") * cameraPanSpeed, Space.World);
         }
 
-
-        /*if (Input.GetKeyDown(KeyCode.F))
-        {
-
-            target = ObjectInspection.next;
-            FocusCameraOnGameObject(Camera.main, target);
-        }*/
 
 
         //Movement with W,A,S,D
@@ -75,27 +63,4 @@ public class CamControl : MonoBehaviour
         //Zoom in and out with Mouse Wheel
         transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, Space.Self);
     }
-
-
-
-
-    /*Bounds CalculateBounds(GameObject go)
-    {
-        Bounds b = new Bounds(go.transform.position, Vector3.zero);
-        Object[] rList = go.GetComponentsInChildren(typeof(Renderer));
-        foreach (Renderer r in rList)
-        {
-            b.Encapsulate(r.bounds);
-        }
-        return b;
-    }*/
-
-    /*void FocusCameraOnGameObject(Camera c, GameObject go)
-    {
-        Bounds b = CalculateBounds(go);
-        Vector3 max = b.size;
-        float radius = Mathf.Max(max.x, Mathf.Max(max.y, max.z));
-        float dist = radius / (Mathf.Sin(c.fieldOfView * Mathf.Deg2Rad / 2f));
-        c.transform.position = go.transform.position + transform.rotation * Vector3.forward * -dist;
-    }*/
 }
