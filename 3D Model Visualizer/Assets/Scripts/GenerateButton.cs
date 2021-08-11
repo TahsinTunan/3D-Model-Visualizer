896c0d6500c883d5d8e5cb6d70fbd1781b9f0e8b
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-08-11 22:19:19 +0600

    Generate Button

--- 3D Model Visualizer/Assets/Scripts/GenerateButton.cs
+++ 3D Model Visualizer/Assets/Scripts/GenerateButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,7 +30,5 @@ public class GenerateButton : MonoBehaviour
 
 
     void Update()
-    {
-
-    }
+    { }
 }
